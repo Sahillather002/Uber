6564f3f4df3f697c7f4ded17cbfbf534bf27aa7e
Sahil Lather <sahillather002@gmail.com>
2023-07-06 09:13:57 +0530

    input done on search yoohoo....

--- pages/search.js
+++ pages/search.js
@@ -14,6 +14,13 @@ const search = () => {
             <Line src="https://img.icons8.com/?size=512&id=118831&format=png"/>
             <Square src="https://img.icons8.com/?size=512&id=12946&format=png"/>
         </FromToIcons>
+
+        <InputBoxes>
+        <Input placeholder='  Enter a pickup location'/>
+        <Input placeholder='  Where you want to go'/>
+        </InputBoxes>
+
+        <PlusIcon src="https://img.icons8.com/?size=512&id=3220&format=png"/>
         </InputContainer>
     </Wrapper>
   )
@@ -37,20 +44,45 @@ h-12
 `
 const InputContainer=tw.div`
 bg-white
-
+flex
+items-center
 p-2
 `
 const FromToIcons=tw.div`
 w-10
 flex
 flex-col
+items-center
 `
 const Circle=tw.img`
-h-12
+h-2.5
 `
 const Line=tw.img`
 h-12
 `
 const Square=tw.img`
+h-3
+`
+const InputBoxes=tw.div`
+flex
+flex-col
+flex-1
+
+`
+const Input=tw.input`
+h-10 
+bg-gray-200
+my-2
+rounded-2xl
+p-2
+outline-none
+border-none
+`
+const PlusIcon=tw.img`
 h-12
+w-12
+bg-gray-200
+rounded
+ml-3
+
 `
