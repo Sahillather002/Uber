c8ae209e35fa04f3431fb9c59d629c26b61f59ea
Sahil Lather <sahillather002@gmail.com>
2023-07-08 09:48:00 +0530

    login style is complete baby

---
+++ pages/login.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import tw from "tailwind-styled-components"
+
+const login = () => {
+  return (
+    <Wrapper>
+        <UberLogo src="https://i.ibb.co/ZMhy8ws/uber-logo.png"/>
+        <Title>Log in to access you account</Title>
+        <HeadImg src="https://i.ibb.co/CsV9RYZ/login-image.png"/>
+        <SignInButton>
+            Sign in with Google
+        </SignInButton>
+    </Wrapper>
+  )
+}
+
+export default login
+
+const Wrapper=tw.div`
+flex
+flex-col
+h-screen
+w-screen 
+bg-gray-200
+p-4
+`
+const SignInButton=tw.button`
+bg-black
+text-white
+text-center
+py-4
+mt-8
+self-center
+w-full
+cursor-pointer
+rounded-2xl
+`
+const UberLogo=tw.img`
+h-20
+w-auto
+object-contain
+self-start
+`
+const Title=tw.div`
+text-5xl
+pt-4
+text-gray-500
+`
+const HeadImg=tw.img`
+h-screen
+`
